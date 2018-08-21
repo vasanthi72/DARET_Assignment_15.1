6953321c1d5a668c7982dd37842149afae2057e7
vasanthi72 <40771480+vasanthi72@users.noreply.github.com>
2018-08-21 16:57:21 +0530

    Add files via upload

---
+++ Asst15.1.R
@@ -0,0 +1,58 @@
+Features_Variant_1 <- read.csv("E:/kamagyana/Computing/DARET/Assignments/Dataset/Training/Features_Variant_1.csv", header=FALSE, stringsAsFactors=FALSE)
+View(Features_Variant_1)
+FV1 <- Features_Variant_1
+nrow(FV1)
+ncol(FV1)
+sum(is.na(FV1))
+Features_Variant_2 <- read.csv("E:/kamagyana/Computing/DARET/Assignments/Dataset/Training/Features_Variant_2.csv", header=FALSE, stringsAsFactors=FALSE)
+View(Features_Variant_2)
+FV2 <- Features_Variant_2
+nrow(FV2)
+ncol(FV2)
+sum(is.na(FV2))
+Features_TestSet <- read.csv("E:/kamagyana/Computing/DARET/Assignments/Dataset/Testing/Features_TestSet.csv", header=FALSE, stringsAsFactors=FALSE)
+View(Features_TestSet)
+test <- Features_TestSet
+nrow(test)
+ncol(test)
+sum(is.na(test))
+colnames(FV1)[c(1:4)] <- c("PPop","PChek","PTalk","PCat")
+colnames(FV1)
+colnames(FV1)[c(30:54)] <- c("tcbbt","cl24bbt","c4824bbt","cf24ap","diff4824","bt","Polen","PoShare","PoPromo","H","Psun","Pmon","Ptue","Pwed","Pthu","Pfri","Psat","Bsun","Bmon","Btue","Bwed","Bthu","Bfri","Bsat","ncH")
+colnames(FV1)
+basemodel <- lm(ncH~.,data = FV1)
+summary(basemodel)
+fitmodel <- step(basemodel)
+coefficients(basemodel)
+coefficients(fitmodel)
+AIC(basemodel)
+AIC(fitmodel)
+predFV1 <- predict(fitmodel,FV1)
+msemodel1 <- mean((FV1$ncH - predFV1)^2)
+msemodel1
+colnames(test)[c(1:4)] <- c("PPop","PChek","PTalk","PCat")
+colnames(test)[c(30:54)] <- c("tcbbt","cl24bbt","c4824bbt","cf24ap","diff4824","bt","Polen","PoShare","PoPromo","H","Psun","Pmon","Ptue","Pwed","Pthu","Pfri","Psat","Bsun","Bmon","Btue","Bwed","Bthu","Bfri","Bsat","ncH")
+colnames(test)
+testpred <- predict(fitmodel, test)
+msetest <- mean((test$ncH - testpred)^2)
+msetest
+basemodel
+ls(basemodel)
+cor(FV1$ncH,predFV1)
+cor(test$ncH,testpred)
+AIC(testpred)
+minmaxFV1 <- min_max_accuracy(FV1$ncH,predFV1)
+minmaxFV1 <- mean(min(FV1$ncH,predFV1)/max(FV1$ncH,predFV1))
+minmaxFV1
+minmaxFV1 <- mean(min(test$ncH,testpred)/max(test$ncH,testpred))
+minmaxFV1 <- mean(min(FV1$ncH,predFV1)/max(FV1$ncH,predFV1))
+minmaxtest <- mean(min(test$ncH,testpred)/max(test$ncH,testpred))
+minmaxtest
+mapeFV1 <- mean(abs(predFV1-FV1$ncH)/(FV1$ncH))
+mapeFV1
+mapetest <- mean(abs(testpred - test$ncH)/(test$ncH))
+mapetest
+FV1$dev <- (predFV1-FV1$ncH)
+summary(FV1$dev)
+test$dev <- (testpred - test$ncH)
+summary(test$dev)
